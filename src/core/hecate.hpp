5bbdbed876315dc3017068e63d18f07a964e211f
Sam <sam@Sams-MacBook-Pro.local>
2017-04-25 13:58:29 -0400

    adding src

---
+++ src/core/hecate.hpp
@@ -0,0 +1,10 @@
+/*
+// Conglomerated, centralized objects to share state and sensor data
+//
+// Author: Samuel Rosenstein, samr37l@gmail.com
+// Date: 4/25/2017
+//
+// Hecate is a centralized location for the storage of objects that will be
+// accessible by any system. Each object has a Read/Write lock with
+// writer preffered. For access to objects, just include this file.
+*/
