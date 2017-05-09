651339539a3dc75f88de7bb1436b7686c816863b
Sam <smr277@cornell.edu>
2017-05-09 02:26:51 -0400

    working on things, totally going to screw stuff up now

--- src/SHIL/imu.hpp
+++ src/SHIL/imu.hpp
@@ -43,33 +43,4 @@ namespace shil {
     char msg_buffer[255];
   };
 
-
-  // TODO(@smr277): Create the gyro noise model...or determine if needed
-
-  // Gyroscope noise model. Models gyroscope noise through
-  //  w(t) = w^(t) + b(t) + n(t)
-  //  where b(t) is the gyroscope bias and n(t) is white noise.
-  //  Bias is computed by slowly incrementing the bias at a user defined rate
-  class GyroNoiseModel {
-  public:
-    // @arg std_dev: The standard deviation of the white noise in deg/sec.
-    // @arg bias_rate: The rate of the biad increse in millideg/sec.
-    // @arg pos_or_neg: Whether the rate should be positive or negetive.
-    //    true for positive, false for negetive.
-    GyroNoiseModel(double std_dev, int bias_rate, bool pos_or_neg) :
-        generator(std::chrono::system_clock::now().time_since_epoch().count()),
-        distribution(0.0, std_dev),
-        {
-          this->bias_rate = bias_rate;
-        };
-
-    // Creates gyroscope noise following the model presented above.
-    void CreateGyroNoise(core::sensors::Gyro *gyro);
-  private:
-    std::default_random_engine generator;
-    std::normal_distribution<double> distribution;
-    // The rate of bias increase in millideg/sec.
-    int bias_rate;
-  };
-
 } // namespace SHIL
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include "FreeRTOSmacros.hpp"
+#include "SHIL/FreeRTOSmacros.hpp"
 
-#include "hecate.hpp"
-#include "imu.hpp"
+#include "core/hectate/hecate.hpp"
+// #include "imu.hpp"
 
 
 
@@ -11,8 +11,12 @@ using namespace core;
 using namespace core::sensors;
 
 int main (void) {
-  shil::IMU imu(5500);
+  Gyro my_gyro = {static_cast<int32_t>(imu_internal->roll_rate_degps * 1000),
+                  static_cast<int32_t>(imu_internal->pitch_rate_degps * 1000),
+                  static_cast<int32_t>(imu_internal->yaw_rate_degps * 1000) };
+  Hecate::gyro.Write(my_gyro);
+  // shil::IMU imu(5500);
 
-  Thread::StartScheduler();
+  // Thread::StartScheduler();
   return 0;
 }
